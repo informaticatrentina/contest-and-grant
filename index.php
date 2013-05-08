8351abac72dd06eb042daa8fc0cda27bfdae323f
rahul <rahul@incaendo.com>
2013-05-08 13:00:27 +0530

    modification in css
    change image path in custom.css

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/config.php';
 
 require_once($yii);
+require_once(dirname(__FILE__).'/protected/components/Controller.php');
 Yii::createWebApplication($config)->run();

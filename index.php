27cfc32fd9770519b919c71246e377363dea572d
rahul <rahul@incaendo.com>
2013-05-13 19:56:47 +0530

    modified css and controller for add new action(rendering), index.php
    add css in custom.css file for create contest page, add some action in site controller for create contest page

--- index.php
+++ index.php
@@ -4,5 +4,4 @@ $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/config.php';
 
 require_once($yii);
-require_once(dirname(__FILE__).'/protected/components/Controller.php');
 Yii::createWebApplication($config)->run();
--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -8,7 +8,7 @@
  * 
  * Copyright (c) 2013 <ahref Foundation -- All rights reserved.
  * Author: Rahul Tripathi<rahul@incaendo.com>
- * This file is part of <Aggregator>.
+ * This file is part of <Contest and Grants>.
  * This file can not be copied and/or distributed without the express permission of <ahref Foundation.
  */
 
@@ -21,6 +21,69 @@ class SiteController extends Controller {
   * when an action is not explicitly requested by users.
   */
   public function actionIndex() {
-    $this->render('index');
+    $contest = new Contest();
+    $contestInfo = $contest->getContestDetail();
+    $this->render('index', array('contestInfo' => $contestInfo));
+  }
+  
+  /**
+   * actionEntries
+   * 
+   * This function is used for get entries for a contest and get contest detail
+   */
+  public function actionEntries() {
+    $contest = new Contest();
+    $contestInfo = array();
+    $entries = array();
+    $contestId = $this->getArgumentFromUrl('id');
+    $contest->contestId = $contestId;
+    $entries = $contest->getContestSubmission();//p($entries);
+    $contestInfo = $contest->getContestDetail();
+    $this->render('contestEntries', array('entries' => $entries, 'contestInfo' => $contestInfo));
+  }
+  
+  /**
+   * getArgumentFromUrl
+   * 
+   * This function is used for get argument (value) from url 
+   * @param (string) $param
+   * @return $val
+   */
+  private function getArgumentFromUrl($param) {
+    $val = '';
+    if (array_key_exists($param, $_GET) && !empty($_GET[$param])) {
+      $val = $_GET[$param];
+    }
+    return $val;
+  }
+  
+  /**
+   * actionCreateContest
+   * 
+   * This function is used for create contest
+   */
+  public function actionCreateContest() {
+    $contest = new Contest(); 
+    $message = '';
+    $response = array();
+    if (!empty($_FILES['image']['name'])) { 
+      $image = CUploadedFile::getInstanceByName('image');
+      $imageInfo = pathinfo($image->getName());
+      $imageName = $imageInfo['filename'] . generateRandomString(10) .'.'. $imageInfo['extension']; 
+      $imagePath = CONTEST_IMAGE_URL . $imageName;
+      $ret = $image->saveAs('uploads/contestImage/' . $imageName);
+      $imageResize = Yii::app()->image->load('uploads/contestImage/' . $imageName);
+      $imageResize->resize(800, 350, Image::NONE);
+      $imageResize->save();
+      
+      if ($ret == 1) {
+        $response = $contest->createContest($imagePath);
+      }
+    }
+    if (array_key_exists('msg', $response) && $response['msg']) {
+      $message =  $response['msg'];
+    }
+    $this->render('contestCreation', array('message' => $message));
   }
 }
+

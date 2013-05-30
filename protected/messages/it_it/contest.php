35876afc6d8f29bb7b744b0b740397f0e70c0a35
rahul <rahul@incaendo.com>
2013-05-30 10:32:39 +0530

    add contest.php file for message translation
    this file contain english to italian conversion of all message

---
+++ protected/messages/it_it/contest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is used for translate all messages (used in project)
+ * 
+ * Copyright (c) 2013 <ahref Foundation -- All rights reserved.
+  * Author: Rahul Tripathi<rahul@incaendo.com>
+  * This file is part of <Contest and Grants>.
+  * This file can not be copied and/or distributed without the express permission of <ahref Foundation.
+  */
+
+  return array(
+      'Aggregator returning httpcode:' => 'Aggregatore ritorno httpcode:',
+      'Aggregator is not responding or Curl failed' => 'Aggregator non risponde o non è riuscita Curl',
+      'Zero length response not permitted' => 'Zero lunghezza di risposta non è permesso',
+      'Error in curlGet :' => 'Errore in curlGet:',
+      'Return fields should not be empty' => 'Campi di ritorno non devono essere vuoti',
+      'Input data in getEntry : ' => 'I dati di ingresso in getEntry:',
+      'Error in getEntry method :' => 'Errore nel metodo getEntry:',
+      'Error in saveEntry method :' => 'Errore nel metodo saveEntry:',
+      'Start date should not be empty' => 'Data di inizio non deve essere vuoto',
+      'End date should not be empty' => 'Data di fine non deve essere vuoto',
+      'Creation date should not be empty' => 'Data di creazione non deve essere vuoto',
+      'Contest title should not be empty' => 'ontest titolo non deve essere vuoto',
+      'Contest description should not be empty' => 'Descrizione del concorso non deve essere vuoto',
+      'Please provide contest image' => "Si prega di fornire un'immagine contest",
+      'Identitity Manager returning httpcode: ' => 'Identitity Responsabile ritorno httpcode:',
+      'Identitity Manager  is not responding or Curl failed' => 'Identitity Manager non risponde o non è riuscita Curl',
+      'Error in createUser :' => 'Errore in createUser:',
+      'Please enter first name' => 'Inserisci il nome',
+      'Please enter last name' => 'Inserisci il cognome',
+      'Please enter a valid email' => 'Si prega di inserire un indirizzo email valido',
+      'Please enter password' => 'Inserisci la password',
+      'You have successfully created your account' => 'È stato creato il tuo account',
+      'Please try again' => 'Riprova',
+      'Error in createUser method :' => 'Errore nel metodo createUser:',
+      'Please enter a valid email' => 'Si prega di inserire un indirizzo email valido',
+      'You have entered either wrong email id or password. Plery againase try again' => 'È stato immesso sia id mail o password errati. Plery againase riprovare',
+      'Error in validateUser method :' => 'Errore nel metodo actionRegisterUser:',
+      'Error in actionRegisterUser method :' => 'Errore nel metodo actionRegisterUser:',
+      'You have created a contest Succesfully' => 'È stato creato un concorso con success',
+      'Contest title should not be empty' => 'Titolo di concorso non deve essere vuoto',
+      'Please enter valid start date' => 'Inserisci data di inizio valida',
+      'End date should not be empty' => 'Data di fine non deve essere vuoto',
+      'Please enter valid end date' => 'Inserisci data di fine validità',
+      'Contest description should not be empty' => 'Descrizione del concorso non deve essere vuoto',
+      'Please choose an image for upload' => "Scegliere un immagine per l'upload",
+      'This contest title is already exist' => 'Questo titolo concorso è già presente',
+      'Error in createContest :' => 'Errore in createContest:',
+      'You have succesfully submit an entry' => 'Avete successo presentare una voce',
+      'Error in submitContestEntry :' => 'Errore in submitContestEntry:',
+      'Please login to submit an entry' => "Effettua l'accesso per inviare una voce",
+      'Email id already in use, Please choose a different email id' => 'Email id già in uso, Seleziona un altro id e-mail',
+      'Some technical problem occurred, contact administrator' => 'Qualche problema tecnico si è verificato, amministratore di contatto'
+  );

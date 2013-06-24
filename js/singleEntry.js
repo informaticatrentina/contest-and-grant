4b3e340da384b2edbf64af4a68786a8a2a0eaa57
rahul <rahul@incaendo.com>
2013-06-24 13:26:40 +0530

    add facebook like, twitter and pinterest on single entry page
    add facebook like, twitter and pinterest on single entry page after photo description

--- js/singleEntry.js
+++ js/singleEntry.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
   $('#home').find("a").removeClass('candgselected');
-  $('#createContest').find("a").addClass('candgselected');  
+  $('#createContest').find("a").addClass('candgselected');
+
+ !function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }
+  }
+  (document, "script", "twitter-wjs"); 
 });

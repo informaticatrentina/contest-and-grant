ca4e13cc772c15e48a27d6f355466915d940e8b5
rahul <rahul@incaendo.com>
2013-09-11 12:44:26 +0530

    Remove winner part on single entry page
    Remove winner part on single entry page if winner status is hide

--- protected/controllers/ContestController.php
+++ protected/controllers/ContestController.php
@@ -120,6 +120,7 @@ class ContestController extends Controller {
       if (!empty($entries)) {
         $entry['contestTitle'] = $contestInfo['contestTitle'];
         $entry['contestSlug'] = $contestInfo['contestSlug'];
+        $entry['winnerStatus'] = $contestInfo['winnerStatus'];
         $entry['title'] = $entries['title'];
         $entry['description'] = $entries['content']['description'];
         $entry['authorName'] = $entries['author']['name'];

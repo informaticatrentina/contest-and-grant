3292c947e22f348b384a6c0b6c7fe4a00d87177c
rahul <rahul@incaendo.com>
2013-11-29 18:25:31 +0530

    change migration filename for alter jury table

--- protected/migrations/m131125_105700_alter_contest_for_jury_rating.php
+++ protected/migrations/m131125_105700_alter_contest_for_jury_rating.php
@@ -1,6 +1,6 @@
 <?php
 
-class m130906_050040_alter_contest_table extends CDbMigration {
+class m131125_105700_alter_contest_for_jury_rating extends CDbMigration {
 
   public function up() {
     $this->execute("ALTER TABLE `contest` ADD `jury_rating_from` DATETIME NOT NULL ,
